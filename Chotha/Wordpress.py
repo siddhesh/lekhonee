f161c1540ea3fb590c780cba9b8e0efd4b0285d9
Kushal Das <kushal@fedoraproject.org>
2009-05-13 17:48:55 +0530

    UI and Wordpress class changed to support editing

--- Chotha/Wordpress.py
+++ Chotha/Wordpress.py
@@ -3,7 +3,7 @@
 #       Author : Kushal Das
 #       Copyright (c)  2007 Kushal Das
 #       kushal@fedoraproject.org
-#       
+#
 #       Copyright: See COPYING file that comes with this distribution
 #
 #
@@ -20,12 +20,21 @@ class Wordpress(BlogServer):
         """Post the content"""
         postid = self.server.metaWeblog.newPost(1, self.username, self.password, content, publish)
         res = "The blog is updated with postid " + str(postid)
-        return res 
+        return res
+
+    def edit(self, postid, content, publish):
+        """Edit content"""
+        res = self.server.metaWeblog.editPost(postid, self.username, self.password, content, publish)
+        if res == True:
+            return "Post updated"
+        else:
+            return res
+
 
     def getCategories(self):
         """return the list of categories or tags"""
         return self.server.metaWeblog.getCategories(1, self.username, self.password)
-    
+
     def addPage(self, content, publish):
         """Add a new page with content"""
         self.server.wp.newPage(1, self.username, self.password, content, publish)
@@ -33,3 +42,7 @@ class Wordpress(BlogServer):
     def addCategory(self,category):
         """Add a new category"""
         self.server.wp.newCategory(1,self.username, self.password, category)
+
+    def getLastPost(self):
+        """Get the last post"""
+        return self.server.metaWeblog.getRecentPosts(1,self.username,self.password,1)
