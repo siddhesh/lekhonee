8e168f6b1ce9cfa3c27d2deed57b17206656af2f
Kushal Das <kushal@fedoraproject.org>
2009-05-13 15:45:16 +0530

    use kwallet or it will ask details everytime it runs

--- Chotha/ConfigureWinUI.py
+++ Chotha/ConfigureWinUI.py
@@ -21,49 +21,63 @@ from ConfigureWinui import *
 
 
 class ConfigureWinUI(QtGui.QDialog, Ui_ConfigureWin):
-    def __init__(self,parent, wallet):
+    def __init__(self,parent, wallet, start=0):
         QtGui.QDialog.__init__(self)
         self.setupUi(self)
+        if start != 0:
+            self.cancelBttn.setEnabled(False)
         self.wallet = wallet
         self.parent = parent
         self.connect(self.saveBttn,QtCore.SIGNAL("clicked()"),self.save)
         self.connect(self.cancelBttn,QtCore.SIGNAL("clicked()"),self.hide)
+        if self.wallet:
+            if not self.wallet.setFolder('lekhonee'):
+                self.wallet.createFolder('lekhonee')
+                self.wallet.setFolder('lekhonee')
+                self.wallet.writeEntry('username','admin')
+                self.wallet.writeEntry('site','http://yoursite.com/xmlrpc.php')
+                self.wallet.writePassword('http://yoursite.com/xmlrpc.php','changeme')
 
-        if not self.wallet.setFolder('lekhonee'):
-            self.wallet.createFolder('lekhonee')
-            self.wallet.setFolder('lekhonee')
-            self.wallet.writeEntry('username','admin')
-            self.wallet.writeEntry('site','http://yoursite.com/xmlrpc.php')
-            self.wallet.writePassword('http://yoursite.com/xmlrpc.php','changeme')
+            x = QtCore.QByteArray()
+            try:
+                self.wallet.readEntry('username',x)
+            except:
+                self.wallet.writeEntry('username','admin')
+                self.wallet.readEntry('username',x)
+            self.username = str(x)
+            server = QtCore.QByteArray()
+            try:
+                self.wallet.readEntry('site',server)
+            except:
+                self.wallet.writeEntry('site','http://yoursite.com/xmlrpc.php')
+                self.wallet.readEntry('site',server)
+            self.server = str(server)
+            self.password = QtCore.QString()
+            self.wallet.readPassword(self.server,self.password)
+            self.serverTxt.setText(self.server)
+            self.usernameTxt.setText(self.username)
+            self.passwordTxt.setText(self.password)
+        else:
+            self.serverTxt.setText(self.parent.Server)
+            self.usernameTxt.setText(self.parent.username)
+            self.passwordTxt.setText(self.parent.password)
 
-        x = QtCore.QByteArray()
-        try:
-            self.wallet.readEntry('username',x)
-        except:
-            self.wallet.writeEntry('username','admin')
-            self.wallet.readEntry('username',x)
-        self.username = str(x)
-        server = QtCore.QByteArray()
-        try:
-            self.wallet.readEntry('site',server)
-        except:
-            self.wallet.writeEntry('site','http://yoursite.com/xmlrpc.php')
-            self.wallet.readEntry('site',server)
-        self.server = str(server)
-        self.password = QtCore.QString()
-        self.wallet.readPassword(self.server,self.password)
-        self.serverTxt.setText(self.server)
-        self.usernameTxt.setText(self.username)
-        self.passwordTxt.setText(self.password)
 
     def save(self):
-        """Don't know why this method is doing what it supposed to do"""
+        """save details if using kwallet"""
         self.username = str(self.usernameTxt.text())
         self.password = str(self.passwordTxt.text())
         self.server = str(self.serverTxt.text())
-        self.wallet.writeEntry('username',self.username)
-        self.wallet.writeEntry('site',self.server)
-        self.wallet.writePassword(self.server,self.password)
-        self.parent.reloadInfo()
+        if self.wallet:
+            self.wallet.writeEntry('username',self.username)
+            self.wallet.writeEntry('site',self.server)
+            self.wallet.writePassword(self.server,self.password)
+            self.parent.reloadInfo()
+        else:
+            self.parent.username = self.username
+            self.parent.password = self.password
+            self.parent.Server = self.server
+            self.parent.makeServer()
+            self.parent.getCategories()
 
         self.hide()
