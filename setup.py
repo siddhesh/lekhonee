ee44f784a2c5b76edcd1cd2f0d6c284922221b18
Kushal Das <kushal@fedoraproject.org>
2009-06-15 11:29:42 +0530

    gnome .desktop file

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-"""Lekhonee is a PyQt4 based Wordpress blog client. It is usefull for low bandwidth based connections"""
+"""Lekhonee is a desktop Wordpress blog client. It is usefull for low bandwidth based connections"""
 from distutils.core import setup
 
 doclines = __doc__.split("\n")
 setup(name='lekhonee',
-      version='0.3.1',
+      version='0.4',
       description=doclines[0],
       long_description = "\n".join(doclines[:]),
       platforms = ["Linux"],
       author='Kushal Das',
       author_email='kushal@fedoraproject.org',
-      url='http://sourceforge.net/projects/chotha',
+      url='http://fedorahosted.org/lekhonee',
       license = 'http://www.gnu.org/copyleft/gpl.html',
       packages=['Chotha'],
       data_files=[('/usr/share/applications',['pixmaps/lekhonee.desktop']),
@@ -19,5 +19,6 @@ setup(name='lekhonee',
 		  ('/usr/share/pixmaps',['pixmaps/lekhonee.png', 'pixmaps/chothasplash.png']),
 		  ('/usr/share/chotha/docs',['docs/README','docs/LICENSE','docs/COPYING','docs/ChangeLog']),
           ('/usr/share/chotha/ui',['ui/ConfigureWin.ui', 'ui/ImageDialog.ui', 'ui/Lekhonee.ui', 'ui/OldEntryDialog.ui' ]),
-          ('/usr/share/pixmaps/chotha',['icons/application-exit.png', 'icons/bold.png', 'icons/configure.png', 'icons/dialog-cancel.png', 'icons/document-open.png', 'icons/document-save.png', 'icons/draft.png', 'icons/insert-image.png', 'icons/internet.png', 'icons/italic.png', 'icons/konqueror.png', 'icons/ok.png', 'icons/subscript.png', 'icons/superscript.png', 'icons/underline.png', 'icons/document-new.png', 'icons/addpage.png'])]
+          ('/usr/share/pixmaps/chotha',['icons/application-exit.png', 'icons/bold.png', 'icons/configure.png', 'icons/dialog-cancel.png', 'icons/document-open.png', 'icons/document-save.png', 'icons/draft.png', 'icons/insert-image.png', 'icons/internet.png', 'icons/italic.png', 'icons/konqueror.png', 'icons/ok.png', 'icons/subscript.png', 'icons/superscript.png', 'icons/underline.png', 'icons/document-new.png', 'icons/addpage.png']),
+          ('/usr/share/chotha/lekhonee-gnome',['lekhonee-gnome/lekhonee-gnome.py','lekhonee-gnome/lekhonee-gnome.glade','lekhonee-gnome/lekhonee-gnome.gladep','lekhonee-gnome/draft.png','lekhonee-gnome/insert-image.png','lekhonee-gnome/internet.png'])]
       )
